06e766d56262c1062584460c007362c3ae0fbc3b
davichete <davidgmorillop@gmail.com>
2022-04-12 15:49:13 +0200

    Minor change to favor DRY

--- webpage/use_cases/retrieve_most_popular_movies.py
+++ webpage/use_cases/retrieve_most_popular_movies.py
@@ -1,18 +1,17 @@
 from typing import Iterator
+
 import bs4
 import httpx
 from webpage.entities.movie import RetrievedMovie
 from webpage.use_cases.retrieve_movie import retrieve_movie
+from webpage.utils.constants import BASE_URL
 
 
 def retrieve_most_popular_movies() -> Iterator[RetrievedMovie]:
-    url = "https://www.imdb.com/chart/moviemeter/"
+    url = BASE_URL + "/chart/moviemeter/"
     response = httpx.get(url)
 
     soup = bs4.BeautifulSoup(response.content, "html.parser")
 
-    links = [
-        f'https://www.imdb.com{tag["href"]}'
-        for tag in soup.select("td.titleColumn a")[:10]
-    ]
+    links = [BASE_URL + tag["href"] for tag in soup.select("td.titleColumn a")[:10]]
     return map(retrieve_movie, links)
