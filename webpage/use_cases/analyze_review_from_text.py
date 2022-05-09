a3c40454ac13bbb1745b52acded4bf24062edcc0
davichete <davidgmorillop@gmail.com>
2022-05-09 17:56:57 +0200

    Deleted unused print

--- webpage/use_cases/analyze_review_from_text.py
+++ webpage/use_cases/analyze_review_from_text.py
@@ -6,7 +6,6 @@ class AnalyzeReviewFromText:
 
     def __init__(self):
         if AnalyzeReviewFromText.model is None:
-            print("how many times?")
             AnalyzeReviewFromText.model = joblib.load("model.joblib")
 
     def analyze(self, text: str) -> bool:
